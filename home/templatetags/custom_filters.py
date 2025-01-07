ab5ae57715621e3dffb1ba3f0d7c418663b5bf02
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2025-01-07 23:05:34 +0000

    clean - add icon

--- home/templatetags/custom_filters.py
+++ home/templatetags/custom_filters.py
@@ -3,10 +3,11 @@ from django import template
 
 register = template.Library()
 
+
 # فیلتر برای تبدیل تاریخ میلادی به شمسی
 @register.filter
 def jalali_date(value):
     if value:
         # تبدیل تاریخ به شمسی
-        return jdatetime.date.fromgregorian(date=value).strftime('%d %B')
+        return jdatetime.date.fromgregorian(date=value).strftime("%d %B")
     return value
