3d0a9da421f120ef7aba31a080af0b0723638077
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2025-01-07 22:30:53 +0000

    news better front - content better content - add dividar

---
+++ home/templatetags/custom_filters.py
@@ -0,0 +1,12 @@
+import jdatetime
+from django import template
+
+register = template.Library()
+
+# فیلتر برای تبدیل تاریخ میلادی به شمسی
+@register.filter
+def jalali_date(value):
+    if value:
+        # تبدیل تاریخ به شمسی
+        return jdatetime.date.fromgregorian(date=value).strftime('%d %B')
+    return value
