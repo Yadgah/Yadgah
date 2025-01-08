b20da4c1e9954c7d4f50b711044b142a5bd2c17d
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2025-01-08 20:43:52 +0000

    clean & user login and singup create and try for create user profile image

--- home/models.py
+++ home/models.py
@@ -1,5 +1,15 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.user.username
 
 
 class News(models.Model):
