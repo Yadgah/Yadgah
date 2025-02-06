29b963691d865b47eca80cf90454ef57e4ae5f87
Dadmehr Emami <garfoxteam@gmail.com>
2025-02-06 19:39:13 +0000

    Created robots.txt

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -1,8 +1,7 @@
-# home/sitemaps.py
-
 from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
+from django.urls import reverse_lazy
 from .models import Question, News
+
 
 class QuestionSitemap(Sitemap):
     changefreq = "weekly"
@@ -12,30 +11,44 @@ class QuestionSitemap(Sitemap):
         return Question.objects.all()
 
     def lastmod(self, obj):
-        return obj.created_at
+        return obj.updated_at if hasattr(obj, "updated_at") else obj.created_at
 
     def location(self, obj):
-        return reverse('question_detail', args=[str(obj.id)])
+        return reverse_lazy("question_detail", args=[obj.id])
+
 
 class NewsSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.9
 
-    def items(self):
+    def get_queryset(self):
         return News.objects.filter(is_active=True)
 
+    def items(self):
+        return self.get_queryset()
+
     def lastmod(self, obj):
-        return obj.published_at
+        return obj.updated_at if hasattr(obj, "updated_at") else obj.published_at
 
     def location(self, obj):
-        return reverse('news_list')
+        return reverse_lazy("news_list")
+
 
 class StaticViewSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
 
+    static_pages = [
+        "index",
+        "privacy_policy",
+        "mit_license",
+        "rules",
+        "leaderboard",
+        "explore",
+    ]
+
     def items(self):
-        return ['index', 'privacy_policy', 'mit_license', 'rules', 'leaderboard', 'explore']
+        return self.static_pages
 
     def location(self, item):
-        return reverse(item)
+        return reverse_lazy(item)
