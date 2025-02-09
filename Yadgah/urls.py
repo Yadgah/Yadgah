feeff58008d84dd410b85898c5eb6bdeecb7adbe
Dadmehr Emami <garfoxteam@gmail.com>
2025-02-09 11:14:52 +0000

    clean

--- Yadgah/urls.py
+++ Yadgah/urls.py
@@ -22,21 +22,15 @@ from django.urls import include, path
 
 from home import views
 
-# Adds site header, site title, index title to the admin side.
-admin.site.site_header = "Yadgah"
-admin.site.site_title = "Yadgah"
-admin.site.index_title = "Welcome Yadgah"
+# # Adds site header, site title, index title to the admin side.
+# admin.site.site_header = "Yadgah"
+# admin.site.site_title = "Yadgah"
+# admin.site.index_title = "Welcome Yadgah"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login/", views.login_view, name="login"),
-    path("signup/", views.signup_view, name="signup"),
-    path("logout/", views.logout_view, name="logout"),
-    path("profile/", views.profile_view, name="profile"),
-    path("search/", views.search_view, name="search"),
     path("", include("home.urls")),
-    path("create/", views.create_news, name="create_news"),
-    path("edit/<int:news_id>/", views.edit_news, name="edit_news"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
