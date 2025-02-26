0fe122a073a14eb467db46b17005455f4b1a1618
Dadmehr Emami <garfoxteam@gmail.com>
2025-02-26 20:22:32 +0000

    Remove hardcoded SECRET_KEY and use env variable

--- Yadgah/settings.py
+++ Yadgah/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2&wv#ayic@*8l=u-kxmpp^-ly-re%z&t!6$7^11b%e&#_a)d-z"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'some-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
