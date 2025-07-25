e7afd56dc4fb44243b02b6cbc32dd1314cb7a88d
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2025-07-25 23:24:05 +0330

    Update Yadgah/urls.py

--- Yadgah/urls.py
+++ Yadgah/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
 
 from blog.views import post_detail, post_list
 from home import views
